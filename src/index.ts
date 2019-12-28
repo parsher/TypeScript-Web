08df7052cf8b9a15dd593e7d7db3814dbbeec931
parsher <26663229+parsher@users.noreply.github.com>
2019-12-28 21:35:49 +0900

    model

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "new record", age: 0 });
+const user = User.buildUser({ name: "new record", age: 0 });
 
 console.log(user.get('name'))
