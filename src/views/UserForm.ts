364b131d3158cc41cc92d3d02aef2b02a5a2cc55
parsher <26663229+parsher@users.noreply.github.com>
2019-12-30 16:09:12 +0900

    Edit

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,26 +1,21 @@
-import { User } from "../models/User";
-
-export class UserForm {
-  constructor(public parent: Element, public model: User) {
-    this.bindModel();
-  }
-
-  bindModel() {
-    this.model.on("change", () => {
-      this.render();
-    });
-  }
-
+import { User, UserProps } from "../models/User";
+import { View } from "./View";
+export class UserForm extends View<User, UserProps> {
   eventsMap(): { [key: string]: () => void } {
     return {
       "click:.set-age": this.onSetAgeClick,
-      "click:.set-name": this.onSetNameClick
+      "click:.set-name": this.onSetNameClick,
+      'click:.save-model': this.onSaveClick
     };
   }
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
   };
+
+  onSaveClick = (): void => {
+    this.model.save();
+  }
 
   onSetNameClick = (): void => {
     const input = this.parent.querySelector("input");
@@ -35,34 +30,11 @@ export class UserForm {
     return `
       <div>
         <h1>UserForm</h1>
-        <div>User name: ${this.model.get("name")}</div>
-        <div>User age: ${this.model.get("age")}</div>
-        <input />
+        <input placeholder="${this.model.get("name")}" />
         <button class="set-name">Change Name</button>
         <button class="set-age">Set random age</button>
+        <button class="save-model">Save Model</button>
       </div>
     `;
-  }
-
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap();
-
-    for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(":");
-
-      fragment.querySelectorAll(selector).forEach(element => {
-        element.addEventListener(eventName, eventsMap[eventKey]);
-      });
-    }
-  }
-
-  render(): void {
-    this.parent.innerHTML = "";
-
-    const templateElement = document.createElement("template");
-    templateElement.innerHTML = this.template();
-    this.parent.append(templateElement.content);
-
-    this.bindEvents(templateElement.content);
   }
 }
