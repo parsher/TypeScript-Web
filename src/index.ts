03a3c715fc5f3f36ba05b2ef86ff90fd31894e40
parsher <26663229+parsher@users.noreply.github.com>
2019-12-30 16:22:29 +0900

    Collection

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
-import { UserForm } from "./views/UserForm";
-import { User } from "./models/User";
+// import { UserEdit } from "./views/UserEdit";
+// import { User } from "./models/User";
 
-const user = User.buildUser({ name: "NAME", age: 20 });
+// const user = User.buildUser({ name: "NAME", age: 20 });
 
-const root = document.getElementById("root");
+// const root = document.getElementById("root");
 
-if (root) {
-  const userForm = new UserForm(root, user);
+// if (root) {
+//   const userEdit = new UserEdit(root, user);
 
-  userForm.render();
-} else {
-  console.log('root element not found');
-}
+//   userEdit.render();
+//   console.log(userEdit);
+// } else {
+//   console.log('root element not found');
+// }
+
+import {UserList} from './views/UserList';
+import {Collection} from './models/Colletion';
+import {UserProps, User} from './models/User';
+
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+  return User.buildUser(json);
+});
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    new UserList(root, users).render();
+  }
+})
+
+users.fetch();
